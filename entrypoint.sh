4def44f1fca238a263919364040cc89c90ea64bd
abs51295 <aagams68@gmail.com>
2018-11-26 12:59:23 +0530

    Fix flask application name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gunicorn --pythonpath /src -b 0.0.0.0:$SERVICE_PORT --workers=2 -k sync -t $SERVICE_TIMEOUT flask_predict:app
+gunicorn --pythonpath /src -b 0.0.0.0:$SERVICE_PORT --workers=2 -k sync -t $SERVICE_TIMEOUT recommendation_service:app
