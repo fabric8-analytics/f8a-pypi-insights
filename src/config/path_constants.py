cd816929d59d13fca609053490e5c04edd1b866c
miteshvp <mitpatel@redhat.com>
2019-01-21 09:10:06 +0530

    introduce versioning for scoring python

--- src/config/path_constants.py
+++ src/config/path_constants.py
@@ -2,7 +2,8 @@
 import os
 
 ECOSYSTEM = os.environ.get('HPF_SCORING_REGION', 'pypi')
-PACKAGE_TO_ID_MAP = os.path.join(ECOSYSTEM, 'scoring/package-to-id-dict.json')
-ID_TO_PACKAGE_MAP = os.path.join(ECOSYSTEM, 'scoring/id-to-package-dict.json')
-MANIFEST_TO_ID_MAP = os.path.join(ECOSYSTEM, 'scoring/manifest-to-id.pickle')
-HPF_MODEL_PATH = os.path.join(ECOSYSTEM, 'model/HPF_model.pkl')
+MODEL_VERSION = os.environ.get('MODEL_VERSION', '2019-01-03')
+PACKAGE_TO_ID_MAP = os.path.join(ECOSYSTEM, MODEL_VERSION, 'scoring/package-to-id-dict.json')
+ID_TO_PACKAGE_MAP = os.path.join(ECOSYSTEM, MODEL_VERSION, 'scoring/id-to-package-dict.json')
+MANIFEST_TO_ID_MAP = os.path.join(ECOSYSTEM, MODEL_VERSION, 'scoring/manifest-to-id.pickle')
+HPF_MODEL_PATH = os.path.join(ECOSYSTEM, MODEL_VERSION, 'model/HPF_model.pkl')
