775e89ce77850f53f7550fc5c0cd2af25b16b62d
miteshvp <mitpatel@redhat.com>
2019-01-31 12:43:15 +0530

    Introduce proper naming conventions for model retraining

--- src/config/path_constants.py
+++ src/config/path_constants.py
@@ -3,7 +3,12 @@ import os
 
 ECOSYSTEM = os.environ.get('HPF_SCORING_REGION', 'pypi')
 MODEL_VERSION = os.environ.get('MODEL_VERSION', '2019-01-03')
-PACKAGE_TO_ID_MAP = os.path.join(ECOSYSTEM, MODEL_VERSION, 'scoring/package-to-id-dict.json')
-ID_TO_PACKAGE_MAP = os.path.join(ECOSYSTEM, MODEL_VERSION, 'scoring/id-to-package-dict.json')
-MANIFEST_TO_ID_MAP = os.path.join(ECOSYSTEM, MODEL_VERSION, 'scoring/manifest-to-id.pickle')
-HPF_MODEL_PATH = os.path.join(ECOSYSTEM, MODEL_VERSION, 'model/HPF_model.pkl')
+DEPLOYMENT_PREFIX = os.environ.get('DEPLOYMENT_PREFIX', 'dev')
+PACKAGE_TO_ID_MAP = os.path.join(ECOSYSTEM, DEPLOYMENT_PREFIX, MODEL_VERSION,
+                                 'trained-model/package-to-id-dict.json')
+ID_TO_PACKAGE_MAP = os.path.join(ECOSYSTEM, DEPLOYMENT_PREFIX, MODEL_VERSION,
+                                 'trained-model/id-to-package-dict.json')
+MANIFEST_TO_ID_MAP = os.path.join(ECOSYSTEM, DEPLOYMENT_PREFIX, MODEL_VERSION,
+                                  'trained-model/manifest-to-id.pickle')
+HPF_MODEL_PATH = os.path.join(ECOSYSTEM, DEPLOYMENT_PREFIX, MODEL_VERSION,
+                              'intermediate-model/HPF_model.pkl')
