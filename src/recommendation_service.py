899d6e20152b572f7b6790e11d0fe605e1bbd178
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-19 09:29:53 +0100

    REST is an abbreviation

--- src/recommendation_service.py
+++ src/recommendation_service.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Defines the rest API for the recommender.
+"""Defines the REST API for the recommender.
 
-Copyright © 2018 Red Hat Inc.
+Copyright © 2018, 2019 Red Hat Inc.
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
