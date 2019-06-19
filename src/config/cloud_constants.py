c74cc6d15394e453e00c1cc318ca34c5d91c9cbb
sunilk747 <ssamal@redhat.com>
2019-06-19 18:41:53 +0530

    pick bucket name from secrets

--- src/config/cloud_constants.py
+++ src/config/cloud_constants.py
@@ -21,12 +21,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import os
 import numpy as np
 
-from src.config.path_constants import DEPLOYMENT_PREFIX
-
 USE_CLOUD_SERVICES = os.environ.get('USE_CLOUD_SERVICES', 'true') == 'true'
 AWS_S3_ACCESS_KEY_ID = os.environ.get('AWS_S3_ACCESS_KEY_ID', '')
 AWS_S3_SECRET_KEY_ID = os.environ.get('AWS_S3_SECRET_ACCESS_KEY', '')
-S3_BUCKET_NAME = DEPLOYMENT_PREFIX + '-' + os.environ.get('S3_BUCKET_NAME', 'hpf-pypi-insights')
+S3_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME', '')
 AWS_S3_ENDPOINT_URL = os.environ.get('AWS_S3_ENDPOINT_URL', '')
 MIN_CONFIDENCE_SCORE = np.float32(int(os.environ.get('MIN_CONFIDENCE_SCORE', 30)))
 GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN', '')
