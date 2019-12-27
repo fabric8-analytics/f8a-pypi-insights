bc4998948177c9e7f627df989cdce14752fdfb5f
anurag trivedi <antrived@localhost.localdomain>
2019-12-27 23:31:59 +0530

    add bayesian sets code and files

---
+++ bayesiansets/In-Prod/predict_model.py
@@ -0,0 +1,293 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This file contains the code that deals with the HPF piece for scoring.
+
+Copyright © 2018 Red Hat Inc.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import logging
+import sys
+import itertools
+
+import daiquiri
+import pandas as pd
+import numpy as np
+import math
+
+from src.config.path_constants import (HPF_MODEL_PATH, PACKAGE_TO_ID_MAP,
+                                       ID_TO_PACKAGE_MAP, MANIFEST_TO_ID_MAP)
+
+from src.config.cloud_constants import MIN_CONFIDENCE_SCORE
+
+daiquiri.setup(level=logging.INFO)
+_logger = daiquiri.getLogger(__name__)
+
+
+class HPFScoring:
+    """This class contains logic for scoring the trained HPF model."""
+
+    def __init__(self, num_recommendations, data_store):
+        """Initialize HPFScoring instance.
+
+        :param num_recommendations: number of recommendations to fetch from the model
+        :param data_store: instance of s3_data_store or local_filesystem
+        """
+        self.m = num_recommendations
+        self.s3_client = data_store
+        self.recommender = self._load_model()
+        self.package_to_id_map = self._load_package_to_id_map()
+        self.id_to_package_map = self._load_id_to_package_map()
+        self.manifest_to_id_map = self._load_manifest_to_id_map()
+
+    def _load_model(self):
+        """Load the model from s3."""
+        return self.s3_client.read_pickle_file(HPF_MODEL_PATH)
+
+    def _load_package_to_id_map(self):
+        """Load package to id map."""
+        return self.s3_client.read_json_file(PACKAGE_TO_ID_MAP).get('package_list')
+
+    def _load_id_to_package_map(self):
+        """Load id to package map."""
+        return self.s3_client.read_json_file(ID_TO_PACKAGE_MAP)
+
+    def _load_manifest_to_id_map(self):
+        """Load manifest to id map."""
+        return self.s3_client.read_pickle_file(MANIFEST_TO_ID_MAP).get('manifest_list')
+
+    def _get_closest_manifest_file(self, input_stack):
+        """Get closest manifest file.
+
+        :param input_stack: Input stack of the user
+        :return manifest_id, exact_match
+        """
+        manifest_id = self.manifest_to_id_map.get(input_stack, -1)
+        exact_match = True
+        if manifest_id == -1:
+            exact_match = False
+            min_diff = sys.maxsize
+            for idx, manifest in enumerate(self.manifest_to_id_map.keys()):
+                diff = len(manifest.difference(input_stack))
+                if input_stack.issubset(manifest) and diff < min_diff:
+                    min_diff = diff
+                    manifest_id = idx
+
+        return manifest_id, exact_match
+
+    def _map_input_to_package_ids(self, input_stack):
+        """Map user input to package ids.
+
+        :param input_stack: User's input stack
+        :return: package_id_list, missing_packages
+        """
+        package_id_list = list()
+        missing_packages = list()
+        for package in input_stack:
+            package_id = self.package_to_id_map.get(package, -1)
+            if package_id == -1:
+                missing_packages.append(package)
+            else:
+                package_id_list.append(package_id)
+
+        return package_id_list, missing_packages
+
+    def _get_packages_from_id(self, package_ids):
+        """Get packages from their ids.
+
+        :param package_ids: list of package ids
+        :return: package_list
+        """
+        package_list = list()
+        for i in package_ids:
+            package = self.id_to_package_map.get(str(i))
+            # We always have all packages recommended by model from the original package list.
+            package_list.append(package)
+        return package_list
+
+    @staticmethod
+    def _sigmoid(array):
+        return 1 / (1 + np.exp(-array))
+
+    def predict(self, input_stack):
+        """Predict companion packages for user stack.
+
+        :param input_stack: user stack
+        :return: companion_packages, missing_packages
+        """
+        user_id, exact_match = self._get_closest_manifest_file(input_stack)
+        package_id_list, missing_packages = self._map_input_to_package_ids(input_stack)
+        companion_packages = list()
+        if len(package_id_list) < len(missing_packages):
+            _logger.info("Number of unknown packages more than known")
+            return companion_packages, missing_packages
+        if user_id == -1:
+            _logger.info("Adding a new user....")
+
+            try:
+                counts_df = pd.DataFrame({
+                    'ItemId': package_id_list,
+                    'Count': [1] * len(package_id_list)
+                })
+                user_id = self.recommender.nusers
+                is_user_added = self.recommender.add_user(
+                    user_id=user_id,
+                    counts_df=counts_df
+                )
+                if is_user_added:
+                    recommendations = self.recommender.topN(
+                        user=user_id,
+                        n=self.m
+                    )
+                else:
+                    raise ValueError('Unable to add user')
+
+            except ValueError as e:
+                _logger.error(e)
+                return companion_packages, missing_packages
+
+        else:
+            if exact_match:
+                _logger.info("Found an exact match")
+            else:
+                _logger.info("Found a closest match")
+            recommendations = self.recommender.topN(
+                user=user_id,
+                n=self.m
+            )
+
+        # Remove packages that were already seen by user.
+        # TODO: Filter packages based on feedback as well.
+        # TODO: Remove transitive dependencies as well.
+        package_id_set = set(package_id_list)
+
+        _logger.info("Input package id set: " + str(package_id_set))
+        _logger.info("Recommendations ids are: " + str(recommendations))
+
+        # Find some better way to do this
+        recommendations = np.array(
+                list(itertools.compress(recommendations,
+                                        [i not in package_id_set for i in recommendations])))
+
+        _logger.info("Filtered recommendation ids are: " + str(recommendations))
+
+        poisson_values = self.recommender.predict(
+            user=[user_id] * recommendations.size,
+            item=recommendations
+        )
+
+        # This is copy pasted on as is basis from maven and NPM model.
+        # It's not the right way of calculating probability by any means.
+        # There is a more lengthier way to calculate probabilities using
+        # logistic regression which remains to be implemented
+        # (but that's also not completely correct).
+        # For discussion please follow: https://github.com/david-cortes/hpfrec/issues/4
+        normalized_poisson_values = HPFScoring._sigmoid(
+            (poisson_values - poisson_values.mean()) / poisson_values.std()) * 100
+
+        filtered_packages = self._get_packages_from_id(recommendations)
+
+        for idx, package in enumerate(filtered_packages):
+            if normalized_poisson_values[idx] >= MIN_CONFIDENCE_SCORE:
+                companion_packages.append({
+                    "package_name": package,
+                    "cooccurrence_probability": str(normalized_poisson_values[idx]),
+                    "topic_list": []
+                })
+
+        return companion_packages, missing_packages
+
+
+class BSScoring:
+
+    def __init__(self, num_recom=None, data_store=None):
+        self.s3_client = data_store
+        self.pack_to_id = self._load_package_to_id_map()
+        self.man_to_id = self._load_manifest_to_id_map()
+        self.id_to_pack = self._load_id_to_package_map()
+        self.model = self._load_model()
+        self.num_recom = num_recom
+
+    def _load_package_to_id_map(self):
+        """Load package to id map."""
+        return self.s3_client.read_json_file(PACKAGE_TO_ID_MAP)
+
+    def _load_manifest_to_id_map(self):
+        """Load manifest to id map."""
+        return self.s3_client.read_pickle_file(MANIFEST_TO_ID_MAP)
+
+    def _load_id_to_package_map(self):
+        """Load id to package map."""
+        return self.s3_client.read_json_file(ID_TO_PACKAGE_MAP)
+
+    def _load_model(self):
+        """Load the model from s3."""
+        return self.s3_client.read_pickle_file(HPF_MODEL_PATH)
+
+    def _get_packages_from_id(self, package_ids):
+        package_list = list()
+        for i in package_ids:
+            package = self.id_to_pack.get(str(i))
+            package_list.append(package)
+        return package_list
+
+    def _get_ids_from_packages(self, input_stack):
+        package_id_list = list()
+        for package in input_stack:
+            package_id = self.pack_to_id.get(package)
+            if package_id is not None:
+                package_id_list.append(package_id)
+        return package_id_list
+
+    def _calculate_recomm_prob(self, recommendations, scores):
+        scores_log = [math.log(i) if i > 0 else 0 for i in scores]
+        scores_log = np.array(scores_log)
+        normalized_scores = (scores_log - scores_log.min()) / (scores_log.max() - scores_log.min())
+        Prob = []
+        for index in recommendations:
+            Prob.append(int(100 * normalized_scores[index]))
+        return Prob
+
+    def predict(self, input_stack):
+        input_stack = self._get_ids_from_packages(input_stack)
+        scores = self.model.query(list(input_stack))
+        ranking = np.argsort(scores)[::-1]
+        top30 = ranking[:30]
+        recommendations = np.array(list(itertools.compress(top30,
+                                                           [i not in input_stack for i in top30])))
+
+        recom = self._get_packages_from_id(recommendations)
+        probs = self._calculate_recomm_prob(recommendations, scores)
+        recom_by_prob = []
+        for i, prob in enumerate(probs):
+            if prob >= 10:
+                #         recom_prob.append(str("{a}({b}%)".format( a=recom[i],b=probs[i] )))
+                recom_by_prob.append(str("{a}".format(a=recom[i])))
+
+        recom_by_prob = [x for i, x in enumerate(recom_by_prob) if i < self.num_recom]
+
+        companion_packages = []
+        for i, rec in enumerate(recom_by_prob):
+            key_val = {}
+            key_val['package_name'] = rec
+            key_val['cooccurrence_probability'] = str(probs[i])
+            key_val['topic_list'] = []
+            companion_packages.append(key_val)
+
+        missing_packages = []
+
+        return companion_packages, missing_packages
+
---
+++ bayesiansets/In-Prod/recommendation_service.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Defines the REST API for the recommender.
+
+Copyright © 2018, 2019 Red Hat Inc.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+import os
+import logging
+
+import daiquiri
+import flask
+from flask import Flask, request
+
+import src.config.cloud_constants as cloud_constants
+from raven.contrib.flask import Sentry
+from src.config.path_constants import ECOSYSTEM
+from rudra.data_store.aws import AmazonS3
+# from src.models.predict_model import HPFScoring
+from src.models.predict_model import BSScoring
+
+app = Flask(__name__)
+
+SENTRY_DSN = os.environ.get("SENTRY_DSN", "")
+sentry = Sentry(app, dsn=SENTRY_DSN, logging=True, level=logging.ERROR)
+app.logger.info('App initialized, ready to roll...')
+
+
+if cloud_constants.USE_CLOUD_SERVICES:
+    s3_client = AmazonS3(bucket_name=cloud_constants.AWS_S3_BUCKET_NAME,
+                         aws_access_key_id=cloud_constants.AWS_S3_ACCESS_KEY_ID,
+                         aws_secret_access_key=cloud_constants.AWS_S3_SECRET_KEY_ID)
+    s3_client.connect()
+
+else:
+    from rudra.data_store.local_data_store import LocalDataStore
+
+    # Change the source directory here for local file system testing.
+    s3_client = LocalDataStore('tests/test_data')
+
+# recommender = HPFScoring(num_recommendations=10, data_store=s3_client)
+recommender = BSScoring(num_recom=30, data_store=s3_client)
+
+daiquiri.setup(level=os.environ.get('FLASK_LOGGING_LEVEL', logging.INFO))
+_logger = daiquiri.getLogger(__name__)
+
+
+@app.route('/api/v1/liveness', methods=['GET'])
+def liveness():
+    """Define the liveness probe."""
+    return flask.jsonify({}), 200
+
+
+@app.route('/api/v1/readiness', methods=['GET'])
+def readiness():
+    """Define the readiness probe."""
+    return flask.jsonify({"status": "ready"}), 200
+
+
+@app.route('/api/v1/companion_recommendation_HPF_Backup', methods=['POST'])
+def recommendation_backup():
+    """Endpoint to serve recommendations."""
+    global recommender
+    limit = 5
+    response_json = []
+    for recommendation_request in request.json:
+        _logger.info("Input direct+transitive package list is......")
+        input_packages = recommendation_request.get('package_list', []) +\
+            recommendation_request.get("transitive_stack", [])
+        _logger.info(input_packages)
+        companions, missing = recommender.predict(
+            input_stack=frozenset(recommendation_request['package_list'])
+        )
+        companions = [d for d in companions if d['package_name'] not in input_packages][:limit]
+        response_json.append({
+            "missing_packages": missing,
+            "companion_packages": companions,
+            "ecosystem": ECOSYSTEM
+        })
+        _logger.info("Sending response.....")
+        _logger.info(response_json)
+    return flask.jsonify(response_json), 200
+
+
+@app.route('/api/v1/companion_recommendation', methods=['POST'])
+def recommendation():
+    """Endpoint to serve recommendations."""
+    global recommender
+    limit = 5
+    response_json = []
+    for recommendation_request in request.json:
+        _logger.info("Input direct+transitive package list is......")
+        input_packages = recommendation_request.get('package_list', []) +\
+            recommendation_request.get("transitive_stack", [])
+        _logger.info(input_packages)
+        companions, missing = recommender.predict(
+            input_stack=frozenset(recommendation_request['package_list'])
+        )
+
+        # image: bayesian-sets
+        companions = [x for i, x in enumerate(companions) if i < limit]
+
+        # image: bayesian-sets-2
+        # companions = [d for d in companions if d['package_name'] not in input_packages][:limit]
+
+        response_json.append({
+            "missing_packages": missing,
+            "companion_packages": companions,
+            "ecosystem": ECOSYSTEM
+        })
+        _logger.info("Sending response.....")
+        _logger.info(response_json)
+    return flask.jsonify(response_json), 200
+
+
+
+
+## Recommendation Service
+# ECOSYSTEM = 'pypi'
+
+
+
+
+
+
+
+
+limit = 5
+response_json = []
+
+_logger.info("Input direct+transitive package list is......")
+# input_packages = recommendation_request.get('package_list', []) +\
+#     recommendation_request.get("transitive_stack", [])
+input_packages = ['pandas', 'numpy', 'datascience', 'folium', 'matplotlib']
+# input_packages = ['numpy', 'keras']
+_logger.info(input_packages)
+
+companions, missing = recommender.predict(input_stack=frozenset(input_packages))
+companions = [x for i, x in enumerate(companions) if i < limit]
+# companions = [d for d in companions if d['package_name'] not in input_packages][:limit]
+
+
+response_json.append({
+    "missing_packages": missing,
+    "companion_packages": companions,
+    "ecosystem": ECOSYSTEM
+})
+_logger.info("Sending response.....")
+_logger.info(response_json)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=6006)
