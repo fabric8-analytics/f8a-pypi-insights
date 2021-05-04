b48b9cf3a3f3e2f2e53e77e4097eee1ea21ade6f
dgpatelgit <dhpatel@redhat.com>
2021-05-04 11:50:59 +0530

    fix: Modified entry script pythonpath to new path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gunicorn --pythonpath /src -b 0.0.0.0:$SERVICE_PORT --workers=2 -k sync -t $SERVICE_TIMEOUT recommendation_service:app
+gunicorn --pythonpath /opt/app-root/src/src -b 0.0.0.0:$SERVICE_PORT --workers=2 -k sync -t $SERVICE_TIMEOUT recommendation_service:app --log-level $FLASK_LOGGING_LEVEL
